5e7c47a2eff9519fe314168a196c3cd86419feab
sashamiliatin <34767366+sashamiliatin@users.noreply.github.com>
2020-09-09 01:58:37 +0300

    inheritance

--- MFCproject/SquareF.cpp
+++ MFCproject/SquareF.cpp
@@ -8,14 +8,3 @@ IMPLEMENT_SERIAL(SquareF, CObject, 1)
 SquareF::SquareF(CPoint P1, CPoint P2) :RectangleF(P1, P2) {
 
 }
-
-void SquareF::Draw(CDC* dc) const {
-	double length = abs(P1.x -P2.x)>abs(P1.y-P2.y)? abs(P1.x - P2.x): abs(P1.y - P2.y);
-	CPen pen1, *oldPen;
-	pen1.CreatePen(PS_SOLID, GetPenSize(), GetPenColor());
-	oldPen = dc->SelectObject(&pen1);
-	CBrush myBrush, *oldBrush;
-	myBrush.CreateSolidBrush(GetBgColor());
-	oldBrush = dc->SelectObject(&myBrush);
-	dc->Rectangle(P1.x, P1.y, length + P1.x, length + P1.y);
-}
--- MFCproject/SquareF.h
+++ MFCproject/SquareF.h
@@ -14,6 +14,13 @@ public:
 		return abs(P1.x - P2.x) > abs(P1.y - P2.y) ? abs(P1.x - P2.x) : abs(P1.y - P2.y);
 	}
 	SquareF(CPoint p1, CPoint p2);
-	void Draw(CDC* dc) const;
+	
+	void Redefine(CPoint p1, CPoint p2)
+	{
+		P1 = p1;
+		double length = abs(p1.x - p2.x) < abs(p1.y - p2.y) ? abs(p1.x - p2.x) : abs(p1.y - p2.y);
+		P2.x = p1.x + length;
+		P2.y = p1.y + length;
+	}
 };
 
